aa73b1140fd9b88e30bfba7237724f7bf499c576
ppmurmu <pradeeppetermurmu@gmail.com>
2021-12-07 14:03:18 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Exampur'),
+      home: MyHomePage(title: 'Exampur App'),
     );
   }
 }
