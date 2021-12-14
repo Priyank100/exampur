e1ae3fd8c4dce141da5a4b7eabd34f658207dbaa
AyushP851 <61340903+AyushP851@users.noreply.github.com>
2021-12-14 23:53:00 +0530

    11.52

--- lib/presentation/widgets/multi_selector.dart
+++ lib/presentation/widgets/multi_selector.dart
@@ -1,98 +1,44 @@
-import 'package:exampur_mobile/presentation/theme/custom_text_style.dart';
+import 'package:exampur_mobile/presentation/widgets/custom_button.dart';
+import 'package:exampur_mobile/presentation/widgets/custom_smaller_button.dart';
+import 'package:exampur_mobile/shared/free_course_card.dart';
+import 'package:exampur_mobile/shared/selection_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_chips_input/flutter_chips_input.dart';
 
-class SelectSubject extends StatefulWidget {
-  final Function(List<String>) value;
-  SelectSubject({Key? key, required this.value}) : super(key: key);
+class MultiSelect extends StatefulWidget {
+  final Function(Set<String>) function;
+  final List<String> options;
+  final Set<String> selected;
+
+  MultiSelect({Key? key, required this.function, required this.options, required this.selected})
+      : super(key: key);
 
   @override
-  _SelectSubjectState createState() => _SelectSubjectState();
+  _MultiSelectState createState() => _MultiSelectState();
 }
 
-class _SelectSubjectState extends State<SelectSubject> {
-  final _chipKey = GlobalKey<ChipsInputState>();
+class _MultiSelectState extends State<MultiSelect> {
   @override
   Widget build(BuildContext context) {
-    const subs = ["Maths", "Biology", "Chemistry"];
-    List<String> selected=[] ;
 
     return Padding(
       padding: EdgeInsets.all(5),
       child: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            RichText(
-              text: TextSpan(
-                style: CustomTextStyle.headingBold(context),
-                text: 'Select Subjects',
+            for (var name in widget.options)
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: InkWell(
+                  onTap: () {
+                    widget.selected.contains(name)? widget.selected.remove(name):widget.selected.add(name);
+                    widget.function(widget.selected);
+                  },
+                  child: SelectionCard(color: widget.selected.contains(name)
+                      ? Colors.yellow
+                      : Colors.black, name: name,),
+                ),
               ),
-            ),
-            SizedBox(height: 20),
-            ChipsInput(
-              key: _chipKey,
-              keyboardAppearance: Brightness.dark,
-              textCapitalization: TextCapitalization.words,
-              textStyle: const TextStyle(
-                  height: 1.5, fontSize: 16),
-              decoration: const InputDecoration(
-
-                labelText: 'Select Subject',
-
-              ),
-              findSuggestions: (String query) {
-
-                if (query.isNotEmpty) {
-                  var lowercaseQuery = query.toLowerCase();
-                  return subs.where((profile) {
-                    return profile
-                        .toLowerCase()
-                        .contains(query.toLowerCase());
-                  }).toList(growable: false)
-                    ..sort((a, b) => a
-                        .toLowerCase()
-                        .indexOf(lowercaseQuery)
-                        .compareTo(
-                        b.toLowerCase().indexOf(lowercaseQuery)));
-                }
-
-                return subs;
-              },
-              onChanged: (data) {
-                selected=data as List<String>;
-                widget.value(selected);
-                print(data);
-
-              },
-              chipBuilder: (context, state, dynamic profile) {
-                return InputChip(
-                  key: ObjectKey(profile),
-                  label: Text(profile),
-                  onDeleted: () => state.deleteChip(profile),
-                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                );
-              },
-              suggestionBuilder: (context, state, dynamic profile) {
-                return ListTile(
-                  key: ObjectKey(profile),
-                  // leading: CircleAvatar(
-                  //   backgroundImage: NetworkImage(profile.imageUrl),
-                  // ),
-                  title: Text(profile),
-
-                  onTap: () => state.selectSuggestion(profile),
-                );
-              },
-            ),
-            SizedBox(height: 20),
-            // ElevatedButton(
-            //   onPressed: () {
-            //     print(selected);
-            //     widget.value(selected);
-            //   },
-            //   child: Text('Submit subject'),
-            // ),
           ],
         ),
       ),
@@ -100,5 +46,3 @@ class _SelectSubjectState extends State<SelectSubject> {
     );
   }
 }
-
-
---
+++ lib/shared/selection_card.dart
@@ -0,0 +1,78 @@
+import 'package:exampur_mobile/presentation/widgets/custom_smaller_button.dart';
+import 'package:exampur_mobile/widgets/custom_outlined_button.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SelectionCard extends StatefulWidget {
+  Color color;
+  String name;
+
+  SelectionCard({
+    Key? key,
+    required this.color,
+    required this.name,
+  }) : super(key: key);
+
+  @override
+  _SelectionCardState createState() => _SelectionCardState();
+}
+
+class _SelectionCardState extends State<SelectionCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(4.0,),
+      child: Wrap(
+        children: [
+          Material(
+            elevation: 10,
+            shadowColor: Colors.black,
+            borderRadius: BorderRadius.all(
+              Radius.circular(20),
+            ),
+            child: Container(
+              width: MediaQuery.of(context).size.width*0.45,
+              decoration: BoxDecoration(
+                color: Colors.white54,
+                border: Border.all(color: widget.color, width: 4),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      // child: Image(
+                      //   image: AssetImage(""),
+                      // ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      widget.name,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+
+
+                  ],
+                ),
+              ),
+            ),
+          ),],
+      ),
+    );
+  }
+}
