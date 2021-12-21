336a28d3568eb47e6f5c279756d33aa33a227d4c
ppmurmu <pradeeppetermurmu@gmail.com>
2021-12-21 16:32:54 +0530

    pre merge

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,14 @@
-
+import 'package:exampur_mobile/presentation/home/bottom_navigation.dart';
 import 'package:exampur_mobile/presentation/home/home.dart';
 import 'package:exampur_mobile/presentation/router/app_router.dart';
 import 'package:exampur_mobile/presentation/theme/themes.dart';
-
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
 
-
-
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
   await FlutterDownloader.initialize(debug: true);
   runApp(const MyApp());
@@ -30,9 +26,6 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   final AppRouter _appRouter = AppRouter();
 
-
-
-
   @override
   Widget build(BuildContext context) {
     final platform = Theme.of(context).platform;
@@ -40,17 +33,14 @@ class _MyAppState extends State<MyApp> {
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
-    return  MaterialApp(
-          title: 'Exampur',
-          theme: CustomTheme.lightTheme,
-          //darkTheme: CustomTheme.darkTheme,
-          debugShowCheckedModeBanner: false,
-          themeMode: ThemeMode.system,
-          onGenerateRoute: _appRouter.onGenerateRoute,
-      home: Home(),
-        );
-
-
-
+    return MaterialApp(
+      title: 'Exampur',
+      theme: CustomTheme.lightTheme,
+      //darkTheme: CustomTheme.darkTheme,
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      onGenerateRoute: _appRouter.onGenerateRoute,
+      home: BottomNavigation(),
+    );
   }
 }
