a22c187e343b43b6af073865030fbaafc342639b
V 130 <ahalawadanchal@gmail.com>
2022-01-04 11:55:02 +0530

    Merged with Anchal 3Jan2022

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -7,7 +7,8 @@ class AppConstants {
   static const String BASE_URL =  'https://6b07f566-12f7-4b32-8f2f-8b6046fa0957.mock.pstmn.io/';
   // static const String BASE_URL2 = 'https://auth.exampur.xyz/';
   static const String BASE_URL2 = 'https://auth.exampur.work/';
-  static const String BASE_URL3 = 'https://static.exampur.xyz/';
+  // static const String BASE_URL3 = 'https://static.exampur.xyz/';
+  static const String BASE_URL3 = 'https://static.exampur.work/';
 
   static const String User_URL            = 'user';
   static const String Login_URL           = BASE_URL2 + 'auth/login';
@@ -15,12 +16,12 @@ class AppConstants {
   static const String Update_User_URL     = BASE_URL2 + 'user';
   static const String Change_Password_URL = BASE_URL2 + 'auth/changePassword';
 
-  static const String homeBanner_URL    = BASE_URL3 + 'banners';
+  static const String homeBanner_URL      = BASE_URL3 + 'banners';
 
-  static const String Courses_URL       = BASE_URL3 + 'courses/free/5/0';
-  static const String e_Books_URL       = BASE_URL3 + 'books/ebook/10/0';
-  static const String One2One_URL       = BASE_URL3 + 'courses/onetoone/10/0';
-  static const String Choose_category_URL =BASE_URL5 + 'app_category';
+  static const String Courses_URL         = BASE_URL3 + 'courses/free/5/0';
+  static const String e_Books_URL         = BASE_URL3 + 'books/ebook/10/0';
+  static const String One2One_URL         = BASE_URL3 + 'courses/onetoone/10/0';
+  static const String Choose_category_URL = BASE_URL3 + 'app_category';
 
 
   static const String Terms_and_Conditions ='https://exampur.com/';
