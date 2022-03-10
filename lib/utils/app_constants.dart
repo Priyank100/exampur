1cd98ba10ae0f477c39e07584c7e44854281171e
V 130 <ahalawadanchal@gmail.com>
2022-03-10 10:25:01 +0530

    Merged with Anchal 10March2022

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -11,56 +11,53 @@ import 'images.dart';
 
 class API {
 
-  static const String BASE_URL = 'https://6b07f566-12f7-4b32-8f2f-8b6046fa0957.mock.pstmn.io/';
-
-  // static const String BASE_URL2 = 'https://auth.exampur.xyz/';
+  static const String BASE_URL  = 'https://6b07f566-12f7-4b32-8f2f-8b6046fa0957.mock.pstmn.io/';
   static const String BASE_URL2 = 'https://auth.exampur.work/';
-
-  // static const String BASE_URL3 = 'https://static.exampur.xyz/';
   static const String BASE_URL3 = 'https://static.exampur.work/';
+  static const String BASE_URL4 = 'http://3.1.205.105:3001/';
 
   //BannerBase
-  static const String BANNER_BASE_URL = 'https://static.exampur.work/get_cdn';
+  static const String BANNER_BASE_URL = BASE_URL3 + 'get_cdn';
 
   //Auth
-  static const String Login_URL = BASE_URL2 + 'auth/login';
-  static const String Valid_Token_URL = BASE_URL2 + 'user';
-  static const String Update_User_URL = BASE_URL2 + 'user';
+  static const String Login_URL           = BASE_URL2 + 'auth/login';
+  static const String Valid_Token_URL     = BASE_URL2 + 'user';
+  static const String Update_User_URL     = BASE_URL2 + 'user';
   static const String Change_Password_URL = BASE_URL2 + 'auth/changePassword';
-  static const String Send_OTP_URL = BASE_URL2 + 'otp';
-  static const String Reset_Password_URL = BASE_URL2 + 'auth/resetPassword';
-  static const String Verify_OTP_URL = BASE_URL2 + 'auth/verification';
+  static const String Send_OTP_URL        = BASE_URL2 + 'otp';
+  static const String Reset_Password_URL  = BASE_URL2 + 'auth/resetPassword';
+  static const String Verify_OTP_URL      = BASE_URL2 + 'auth/verification';
 
   //HomeBanner
-  static const String homeBanner_URL = BASE_URL3 + 'banners';
-  static const String homeBannerCourselink_URL = BASE_URL3 + 'courses/findone/';
-  static const String homeBannerbooklink_URL = BASE_URL3 + 'books/findone/';
+  static const String homeBanner_URL            = BASE_URL3 + 'banners';
+  static const String homeBannerCourselink_URL  = BASE_URL3 + 'courses/findone/';
+  static const String homeBannerbooklink_URL    = BASE_URL3 + 'books/findone/';
 
   //Category
-  static const String Choose_category_URL = BASE_URL3 + 'category/all';
-  static const String Update_Choose_category_URL = BASE_URL2 + 'user/updateCategory';
-  static const String Select_Choose_category_URL = BASE_URL2 + 'user/getCategory';
+  static const String Choose_category_URL         = BASE_URL3 + 'category/all';
+  static const String Update_Choose_category_URL  = BASE_URL2 + 'user/updateCategory';
+  static const String Select_Choose_category_URL  = BASE_URL2 + 'user/getCategory';
 
   //Book/E-Book
-  static const String Books_URL = BASE_URL3 + 'books/printed/10/';
+  static const String Books_URL   = BASE_URL3 + 'books/printed/10/';
   static const String E_Books_URL = BASE_URL3 + 'books/ebook/10/0';
 
   //PaidCourse
-  static const String PaidCoursesTab_URL = BASE_URL3 + 'category/course_paid';
+  static const String PaidCoursesTab_URL  = BASE_URL3 + 'category/course_paid';
   static const String PaidCoursesList_URL = BASE_URL3 + 'courses/paid/' + 'PAID_COURSE_ID' + '/10/';
 
   //FreeCourse
-  static const String FreeCoursesTab_URL = BASE_URL3 + 'category/course_free';
+  static const String FreeCoursesTab_URL  = BASE_URL3 + 'category/course_free';
   static const String FreeCoursesList_URL = BASE_URL3 + 'courses/free/' + 'FREE_COURSE_ID' + '/10/';
 
   //one2one
-  static const String One2One_URL = BASE_URL3 + 'courses/onetoone/10/';
+  static const String One2One_URL         = BASE_URL3 + 'courses/onetoone/10/';
   static const String TermsConditions_URL = 'https://exampur.com/';
 
   //offlineBatches
-  static const String offline_batches = BASE_URL3 + 'offline_centers/findall/10/';
-  static const String offline_batches_center = BASE_URL3 + 'offline_centers/findone/' + 'CENTER_ID' + '/10/';
-  static const String offline_batches_course = BASE_URL3 + 'offline_centers/course/' + 'COURSE_ID';
+  static const String offline_batches         = BASE_URL3 + 'offline_centers/findall/10/';
+  static const String offline_batches_center  = BASE_URL3 + 'offline_centers/findone/' + 'CENTER_ID' + '/10/';
+  static const String offline_batches_course  = BASE_URL3 + 'offline_centers/course/' + 'COURSE_ID';
 
   //appTutorial
   static const String AppTutorial_URL = BASE_URL3 + 'tutorials/app';
@@ -80,7 +77,6 @@ class API {
   static const String CouponCode_URL = BASE_URL2 +'promo_code/';
 
   //demo
- // static const String Demo_URL = BASE_URL3 +'courses/demo/10/';
   static const String Demo_URL = BASE_URL3 +'course_timeline_demo';
 
   //jobAlerts
@@ -89,11 +85,9 @@ class API {
   static const String job_alerts_details_URL  = BASE_URL3 +'alert/findone/' + 'ALERT_ID';
 
   //current_affairs
-  static const String ca_sm_url     = BASE_URL3 + 'content/findall/' +
-      'CONTENT_CATEGORY_ID' + '/' + 'TYPE' + '/' + 'ENCODE_CATEGORY' + '/10/';
-  // https://static.exampur.work/content/findall/:content_category_id/:type/:category/:limit/:skip
+  static const String ca_sm_url = BASE_URL3 + 'content/findall/' + 'CONTENT_CATEGORY_ID' + '/' + 'TYPE' + '/' + 'ENCODE_CATEGORY' + '/10/';
 
-
+  //CaBytes
   static const String ca_bytes_url   = BASE_URL3+ 'ca_byte/' + 'ENCODE_CATEGORY' + '/10/';
 
   //my_purchase
@@ -107,10 +101,9 @@ class API {
   static const String myCourse_timeline_URL     = BASE_URL3 + 'course_timeline';
   static const String myCourse_notification_URL = BASE_URL3 + 'course_notification';
 
-
   //testSeries
-  static const String livetestSeries_URL = 'http://3.1.205.105:3001/live_testseries';
-  static const String myTestSeries_URL = 'http://3.1.205.105:3001/test_series/621e0782b46ad30fca32614d';
+  static const String livetestSeries_URL  = BASE_URL4 + 'live_testseries';
+  static const String myTestSeries_URL    = BASE_URL4 + 'test_series/621e0782b46ad30fca32614d';
 
 }
 
@@ -119,28 +112,27 @@ class Keys {
 }
 
 class SharedPrefConstants {
-  static const String TOKEN = 'Token';
-  static const String BANNER_BASE_SP = 'Banner_Base';
-  static const String USER_DATA = 'user_data';
-  // static const String USER_paymentdata = 'user_payment';
+  static const String TOKEN           = 'Token';
+  static const String BANNER_BASE_SP  = 'Banner_Base';
+  static const String USER_DATA       = 'user_data';
   static const String CATEGORY_LENGTH = 'category_length';
-  static const String PHONE_VERIFY = 'phone_verify';
+  static const String PHONE_VERIFY    = 'phone_verify';
 }
 
 class AppConstants {
 
-  static bool isPrint = true;
-  static bool isAuth = false;
+  static bool isPrint       = true;
+  static bool isAuth        = false;
   static String BANNER_BASE = '';
 
   static String defaultCountry = 'India';
 
   static String Mobile_number = '9873111552';
-  static String Email_id = 'help@exampur.com';
-  static String Email_sub = 'Feedback';
+  static String Email_id      = 'help@exampur.com';
+  static String Email_sub     = 'Feedback';
   static String playStoreAppUrl = 'https://play.google.com/store/apps/details?id=com.edudrive.exampur';
-  static String androidId = 'com.edudrive.exampur';
-  static String iosId = '';
+  static String androidId     = 'com.edudrive.exampur';
+  static String iosId         = '';
 
   static String shareAppContent = 'Hey check out EXAMPUR App at: ' + playStoreAppUrl;
 
@@ -267,20 +259,20 @@ class AppConstants {
 
 class AppColors {
 
-  static const Color amber = Colors.amber;
-  static const Color black = Colors.black;
-  static const Color white = Colors.white;
-  static const Color white24 = Colors.white24;
-  static const Color white38 = Colors.white38;
-  static const Color white54 = Colors.white54;
-  static const Color red = Colors.red;
-  static const Color green = Colors.green;
-  static const Color yellow = Colors.yellow;
-  static const Color blue = Colors.blue;
-  static const Color orange = Colors.orange;
-  static const Color cyan = Colors.cyan;
-  static const Color blueGrey = Colors.blueGrey;
-  static const Color grey = Colors.grey;
+  static const Color amber       = Colors.amber;
+  static const Color black       = Colors.black;
+  static const Color white       = Colors.white;
+  static const Color white24     = Colors.white24;
+  static const Color white38     = Colors.white38;
+  static const Color white54     = Colors.white54;
+  static const Color red         = Colors.red;
+  static const Color green       = Colors.green;
+  static const Color yellow      = Colors.yellow;
+  static const Color blue        = Colors.blue;
+  static const Color orange      = Colors.orange;
+  static const Color cyan        = Colors.cyan;
+  static const Color blueGrey    = Colors.blueGrey;
+  static const Color grey        = Colors.grey;
   static const Color transparent = Colors.transparent;
 
   static Color grey200 = Colors.grey[200]!;
@@ -293,16 +285,16 @@ class AppColors {
 
   static Color blue300 = Colors.blue[300]!;
 
-  static const Color dark = Color(0xFFd19d0f);
-  static const Color jobAlert = Color(0xFF14739c);
-  static const Color darkOrange = Color(0xFFf76d02);
-  static const Color quiz = Color(0xFF0582f7);
-  static const Color affairs = Color(0xFF7790a6);
+  static const Color dark        = Color(0xFFd19d0f);
+  static const Color jobAlert    = Color(0xFF14739c);
+  static const Color darkOrange  = Color(0xFFf76d02);
+  static const Color quiz        = Color(0xFF0582f7);
+  static const Color affairs     = Color(0xFF7790a6);
   static const Color freeCourses = Color(0xFFf23587);
-  static const Color one2one = Color(0xFF069413);
-  static const Color book = Color(0xFF0b8f8d);
+  static const Color one2one     = Color(0xFF069413);
+  static const Color book        = Color(0xFF0b8f8d);
   static const Color paidCourses = Color(0xFFa807a8);
-  static const Color series = Color(0xFFd44242);
+  static const Color series      = Color(0xFFd44242);
 }
 
 class StringConstant {
