044cb7acc364ea1b318fddf797eb9fbf00ef5f25
flutt <ahalawatanchal@gmail.com>
2022-05-04 17:44:52 +0530

    videoplayer

--- lib/shared/local_video_screen.dart
+++ lib/shared/local_video_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:chewie/chewie.dart';
 import 'package:exampur_mobile/utils/appBar.dart';
 import 'package:exampur_mobile/utils/app_constants.dart';
+import 'package:flick_video_player/flick_video_player.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
@@ -15,60 +16,100 @@ class LocalVideoScreen extends StatefulWidget {
   _LocalVideoScreenState createState() => _LocalVideoScreenState();
 }
 class _LocalVideoScreenState extends State<LocalVideoScreen> {
-  late VideoPlayerController videoPlayerController;
-  ChewieController? chewieController;
+  // late VideoPlayerController videoPlayerController;
+  // ChewieController? chewieController;
 
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   // videoPlayerController = VideoPlayerController.file(widget.file);
+  //   // chewieController = ChewieController(
+  //   //   videoPlayerController: videoPlayerController,
+  //   //   aspectRatio: 16/9 ,
+  //   //   autoPlay: true,
+  //   //   looping: true,
+  //   // );
+  //   initializePlayer();
+  // }
+  //
+  // Future<void> initializePlayer() async {
+  //   // videoPlayerController = VideoPlayerController.network('https://assets.mixkit.co/videos/preview/mixkit-forest-stream-in-the-sunlight-529-large.mp4');
+  //   videoPlayerController = VideoPlayerController.file(widget.file);
+  //   await Future.wait([
+  //     videoPlayerController.initialize(),
+  //   ]);
+  //   chewieController = ChewieController(
+  //       videoPlayerController: videoPlayerController,
+  //       autoPlay: true,
+  //       looping: true,
+  //       aspectRatio: 16 / 9,
+  //       // Try playing around with some of these other options:
+  //
+  //       // showControls: false,
+  //       // materialProgressColors: ChewieProgressColors(
+  //       //   playedColor: Colors.red,
+  //       //   handleColor: Colors.blue,
+  //       //   backgroundColor: Colors.grey,
+  //       //   bufferedColor: Colors.lightGreen,
+  //       // ),
+  //       // placeholder: Container(
+  //       //   color: Colors.grey,
+  //       // ),
+  //       // autoInitialize: true,
+  //       errorBuilder: (context, errorMessage) {
+  //         return Center(
+  //           child: Text(
+  //             errorMessage,
+  //             style: TextStyle(color: AppColors.white),
+  //           ),
+  //         );}
+  //   );
+  //   setState(() {});
+  // }
+  // @override
+  // void dispose() {
+  //   videoPlayerController.dispose();
+  //   chewieController!.dispose();
+  //   super.dispose();
+  // }
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: CustomAppBar(),
+  //     body: Column(
+  //       crossAxisAlignment: CrossAxisAlignment.start,
+  //       children: [
+  //         Container(
+  //           padding: EdgeInsets.only(top: 8),
+  //           height: (MediaQuery.of(context).size.width)/16*9,
+  //           child: chewieController != null &&
+  //               chewieController!
+  //                   .videoPlayerController.value.isInitialized
+  //               ? Chewie(
+  //             controller: chewieController!,
+  //           )
+  //               :   Center(child: CircularProgressIndicator(color: AppColors.amber,)),
+  //         ),
+  //         Padding(
+  //           padding: const EdgeInsets.all(8.0),
+  //           child:   Text(widget.title,style: TextStyle(fontSize: 20),),
+  //         )
+  //       ],
+  //     ),
+  //   );
+  // }
+  FlickManager? flickManager;
   @override
   void initState() {
     super.initState();
-    // videoPlayerController = VideoPlayerController.file(widget.file);
-    // chewieController = ChewieController(
-    //   videoPlayerController: videoPlayerController,
-    //   aspectRatio: 16/9 ,
-    //   autoPlay: true,
-    //   looping: true,
-    // );
-    initializePlayer();
-  }
-
-  Future<void> initializePlayer() async {
-    // videoPlayerController = VideoPlayerController.network('https://assets.mixkit.co/videos/preview/mixkit-forest-stream-in-the-sunlight-529-large.mp4');
-    videoPlayerController = VideoPlayerController.file(widget.file);
-    await Future.wait([
-      videoPlayerController.initialize(),
-    ]);
-    chewieController = ChewieController(
-        videoPlayerController: videoPlayerController,
-        autoPlay: true,
-        looping: true,
-        aspectRatio: 16 / 9,
-        // Try playing around with some of these other options:
-
-        // showControls: false,
-        // materialProgressColors: ChewieProgressColors(
-        //   playedColor: Colors.red,
-        //   handleColor: Colors.blue,
-        //   backgroundColor: Colors.grey,
-        //   bufferedColor: Colors.lightGreen,
-        // ),
-        // placeholder: Container(
-        //   color: Colors.grey,
-        // ),
-        // autoInitialize: true,
-        errorBuilder: (context, errorMessage) {
-          return Center(
-            child: Text(
-              errorMessage,
-              style: TextStyle(color: AppColors.white),
-            ),
-          );}
+    flickManager = FlickManager(
+      videoPlayerController:
+      VideoPlayerController.file(widget.file),
     );
-    setState(() {});
   }
+
   @override
   void dispose() {
-    videoPlayerController.dispose();
-    chewieController!.dispose();
+    flickManager!.dispose();
     super.dispose();
   }
   @override
@@ -81,13 +122,9 @@ class _LocalVideoScreenState extends State<LocalVideoScreen> {
           Container(
             padding: EdgeInsets.only(top: 8),
             height: (MediaQuery.of(context).size.width)/16*9,
-            child: chewieController != null &&
-                chewieController!
-                    .videoPlayerController.value.isInitialized
-                ? Chewie(
-              controller: chewieController!,
-            )
-                :   Center(child: CircularProgressIndicator(color: AppColors.amber,)),
+            child: FlickVideoPlayer(
+                flickManager: flickManager!
+            ),
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
@@ -97,4 +134,5 @@ class _LocalVideoScreenState extends State<LocalVideoScreen> {
       ),
     );
   }
+
 }
