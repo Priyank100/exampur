a904bff467ac2355a0e2ea4f1986fec061a0b0f0
flutt <ahalawatanchal@gmail.com>
2022-05-30 11:22:48 +0530

    30-5

---
+++ lib/presentation/home/current_affairs_new/current_affairs_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CurrentAffairsList extends StatefulWidget {
+  const CurrentAffairsList({Key? key}) : super(key: key);
+
+  @override
+  State<CurrentAffairsList> createState() => _CurrentAffairsListState();
+}
+
+class _CurrentAffairsListState extends State<CurrentAffairsList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
