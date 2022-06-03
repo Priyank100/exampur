303d4ed8bca45b413887baab0b59a40648215e24
flutt <ahalawatanchal@gmail.com>
2022-06-03 15:45:00 +0530

    3-6

--- lib/presentation/home/test_series_new/test_series_new.dart
+++ lib/presentation/home/test_series_new/test_series_new.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+
+import 'package:exampur_mobile/presentation/theme/custom_text_style.dart';
 import 'package:exampur_mobile/utils/appBar.dart';
+import 'package:exampur_mobile/utils/app_constants.dart';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class TestSeriesNew extends StatefulWidget {
   const TestSeriesNew({Key? key}) : super(key: key);
@@ -9,15 +14,25 @@ class TestSeriesNew extends StatefulWidget {
 }
 
 class _TestSeriesNewState extends State<TestSeriesNew> {
+  bool isLoading=true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-        Text('POPULAR TEST SERIES FOR BANKING, SSC, RRB & ALL OTHER GOVT. EXAMS')
-      ],),
-    );
+        appBar: CustomAppBar(),
+        body:  Stack(
+          children: <Widget>[
+            WebView(
+              initialUrl: 'https://exampur.com/testseries/',
+              javascriptMode: JavascriptMode.unrestricted,
+              onPageStarted: (start) {
+                setState(() {
+                  isLoading = false;
+                });
+              },
+            ),
+            isLoading ? Center( child: CircularProgressIndicator(color: AppColors.amber,),)
+                : Stack(),
+          ],
+        ),);
   }
 }
