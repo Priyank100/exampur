6d465c538f84443444ee8d584e15389c557601b8
Priyank <anchalahalawat@gmail.com>
2022-08-17 16:59:44 +0530

    17_Aug_2022

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -30,7 +30,7 @@ class SharedPrefConstants {
 
 class AppConstants {
 
-  static bool isPrint       = false;
+  static bool isPrint       = true;
   static bool isotpverify = false;
   static String langCode    = 'en';
   static String filePath    = 'storage/emulated/0/Download/Exampur';
