e8b9f728a083f7b7407fceff13b5bd388d60dbea
Priyank <er.priyank01@gmail.com>
2022-09-13 17:49:39 +0530

    5.15.1+328 13_Sept_2022

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -2,15 +2,6 @@ class API {
   // static const String BASE_URL1 = 'https://auth.exampur.work/';
   // static const String BASE_URL2 = 'https://static.exampur.work/';
 
-  // static const String BASE_URL1 = 'https://5asmwawww1.execute-api.ap-south-1.amazonaws.com/';
-  // static const String BASE_URL2 = 'https://alvf81kry3.execute-api.ap-south-1.amazonaws.com/';
-
-  // static const String BASE_URL1 = 'https://7qclk4stsa.execute-api.ap-south-1.amazonaws.com/';
-  // static const String BASE_URL2 = 'https://seysl9nl99.execute-api.ap-south-1.amazonaws.com/';
-
-  // static const String BASE_URL1 = '';
-  // static const String BASE_URL2 = 'https://alvf81kry3.execute-api.ap-south-1.amazonaws.com/';
-
   //Stage
   // static const String BASE_URL1 = 'https://auth-stage.exampur.xyz/';
   // static const String BASE_URL2 = 'https://static-stage.exampur.xyz/';
@@ -21,9 +12,13 @@ class API {
   // static const String BASE_URL2 = 'https://kubernetes.static.exampur.xyz/';
 
   //cache production
-  static const String BASE_URL1 = 'https://auth.exampurcache.xyz/';
-  // static const String BASE_URL1 = 'http://172.16.12.113:3001/';
-  static const String BASE_URL2 = 'https://static.exampurcache.xyz/';
+  // static const String BASE_URL1 = 'https://auth.exampurcache.xyz/';
+  // static const String BASE_URL2 = 'https://static.exampurcache.xyz/';
+
+  //Testing
+  static const String BASE_URL1 = 'https://5asmwawww1.execute-api.ap-south-1.amazonaws.com/';
+  static const String BASE_URL2 = 'https://alvf81kry3.execute-api.ap-south-1.amazonaws.com/';
+
 
   // call if BASE_URL1 got error-429
   static const String BASE_URL1_U = 'https://qvjplseirk.execute-api.ap-northeast-1.amazonaws.com/';
