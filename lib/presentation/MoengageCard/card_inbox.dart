c1a25fef6b98a844c1aa67d4d5cbb5e7303477e0
priyank100 <er.priyank01@gmail.com>
2023-01-25 12:07:17 +0530

    25Jan2023 - moengage card

---
+++ lib/presentation/MoengageCard/card_inbox.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:moengage_inbox/moengage_inbox.dart';
+
+class CardInbox extends StatefulWidget {
+  const CardInbox({Key? key}) : super(key: key);
+
+  @override
+  State<CardInbox> createState() => _CardInboxState();
+}
+
+class _CardInboxState extends State<CardInbox> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
