ac548f50f413456aeb9b678f9dc81e76584c0d2c
priyank100 <er.priyank01@gmail.com>
2023-01-25 14:15:38 +0530

    25Jan2023 - moengage card

--- lib/presentation/MoengageCard/card_inbox.dart
+++ lib/presentation/MoengageCard/card_inbox.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-// import 'package:moengage_inbox/moengage_inbox.dart';
+import 'package:moengage_inbox/inbox_data.dart';
+import 'package:moengage_inbox/moengage_inbox.dart';
 
 class CardInbox extends StatefulWidget {
   const CardInbox({Key? key}) : super(key: key);
@@ -9,6 +10,20 @@ class CardInbox extends StatefulWidget {
 }
 
 class _CardInboxState extends State<CardInbox> {
+  MoEngageInbox _moEngageInbox = MoEngageInbox('');
+  InboxData? data;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getCards();
+  }
+
+  Future<void> getCards() async {
+    data = await _moEngageInbox.fetchAllMessages();
+    print('>>>>>>>>>>' + data!.messages.length.toString());
+  }
 
   @override
   Widget build(BuildContext context) {
